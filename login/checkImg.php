0f59da7a66e7c59774e18b961b135bd0cb2b82d5
hhjay <sunhhjay@gmail.com>
2015-04-13 20:31:21 +0800

    面试前天

--- login/checkImg.php
+++ login/checkImg.php
@@ -1,5 +1,6 @@
 <?php 
 	// header("Content-type: text/html; charset=UTF-8");
+	// 明天面试，今天就先不写了吧
 	$word = "一段年少时的爱恋牵出一生的爱恋大学时代的赵默笙阳光灿烂对法学系大才子何以琛一见倾心开朗直率的她拔足倒追终于使才气出众的他为她停留驻足何以笙箫默一个执着于等待和相爱的故事全书约11万字作者顾漫结局温馨完整之外的更完整";
 	function fnWord($data) {//中文字体截取
 	 	$len = mb_strlen($data,"utf-8");
@@ -30,6 +31,5 @@
 		imagepng($img);
 		imagedestroy($img);
 	}
-	// $data = "abcd";
 	showImg($resWord);
 ?>
